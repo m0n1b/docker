f895e9731c42bdb6fd384fd1b71448a615b49480
m0n1b <161561830+m0n1b@users.noreply.github.com>
2024-07-12 04:27:00 -0700

    Update aw.sh

--- aw.sh
+++ aw.sh
@@ -1,14 +1,14 @@
 sudo systemctl stop acunetix
-sudo cp wvsc /home/acunetix/.acunetix/v_240427095/scanner/wvsc
-sudo chown acunetix:acunetix /home/acunetix/.acunetix/v_240427095/scanner/wvsc
-sudo chmod +x /home/acunetix/.acunetix/v_240427095/scanner/wvsc
-sudo rm /home/acunetix/.acunetix/data/license/* 
+sudo cp wvsc /home/acunetix/.acunetix/v_240626115/scanner/wvsc
+sudo chown acunetix:acunetix /home/acunetix/.acunetix/v_240226074/scanner/wvsc
+sudo chmod +x /home/acunetix/.acunetix/v_240626115/scanner/wvsc
+sudo rm /home/acunetix/.acunetix/data/license/*
 sudo cp license_info.json /home/acunetix/.acunetix/data/license/
 sudo cp wa_data.dat /home/acunetix/.acunetix/data/license/
 sudo chown acunetix:acunetix /home/acunetix/.acunetix/data/license/license_info.json
 sudo chown acunetix:acunetix /home/acunetix/.acunetix/data/license/wa_data.dat
 sudo chmod 444 /home/acunetix/.acunetix/data/license/license_info.json
 sudo chmod 444 /home/acunetix/.acunetix/data/license/wa_data.dat
-sudo chattr +i /home/acunetix/.acunetix/data/license/license_info.json 
-sudo chattr +i /home/acunetix/.acunetix/data/license/wa_data.dat
+sudo chattr +i /home/acunetix/.acunetix/data/license/license_info.json
+ sudo chattr +i /home/acunetix/.acunetix/data/license/wa_data.da
 sudo systemctl start acunetix
