f007521cd45b3c7f538a83c6cf91e822b54eaf25
m0n1b <161561830+m0n1b@users.noreply.github.com>
2025-08-25 09:28:57 -0700

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -13,4 +13,4 @@ docker --version
 docker-compose --version
 docker tag 15.2 m0n1bxxxxx549/15.2
 docker run -it -p 3443:3443  --privileged  --cap-add CAP_LINUX_IMMUTABLE  --name awvs15.1 m0n1bxxxxx549/25.1:tagname  /usr/sbin/init
-docker run -it   --privileged  --cap-add CAP_LINUX_IMMUTABLE  --name ag.1 m0n1bxxxxx549/25ag:tagname  /usr/sbin/init
+#docker run -it   --privileged  --cap-add CAP_LINUX_IMMUTABLE  --name ag.1 m0n1bxxxxx549/25ag:tagname  /usr/sbin/init
